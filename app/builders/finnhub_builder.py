4dd17f7a78ba33603763ed9bef263c0522f62ca3
Tung Dinh <tungxd301@gmail.com>
2026-03-24 20:10:22 -0700

    Changes

--- app/builders/finnhub_builder.py
+++ app/builders/finnhub_builder.py
@@ -13,4 +13,5 @@ class FinnhubBuilder:
         raw_news_response = self.finnhub_adapter.get_company_news(
             symbol=symbol, _from=_from, to=to
         )
+        print("News", symbol, raw_news_response)
         return NewsResponse.model_validate(raw_news_response)
